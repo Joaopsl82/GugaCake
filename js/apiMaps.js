fab60f997f51f65961b5509210c5a3e807497f6d
João Vitor <joaopsl82@gmail.com>
2020-09-29 12:30:18 -0300

    Esqueleto finalizado

---
+++ js/apiMaps.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-23.5566262 ,lng: -46.6588142};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 17,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
